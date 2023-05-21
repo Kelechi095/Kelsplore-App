9405a95fb5ca79f96e7b1f3fce07bfd79a884be8
Awoms Kelechi <awomskelechi@gmail.com>
2023-05-21 10:47:43 +0100

    Add text animation on the home page

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,13 +1,32 @@
 import "./homePage.css";
 import Navbar from "../../components/navbar/Navbar";
 import { Link } from "react-router-dom";
+import { Typewriter } from "react-simple-typewriter";
+
 
 const HomePage = () => {
   return (
     <main className="home-page">
       <Navbar />
       <section>
-        <h2>MINIMALIST PORTFOLIO SITE</h2>
+        <h2>
+        <Typewriter
+            words={[
+              "MINIMALIST WEBSITE",
+              "EXPLORE TRENDING MOVIES",
+              "CHECK OUT MY FAVORITE BOOKS",
+              "REACH OUT ME!",
+            ]}
+            loop={5}
+            cursor
+            cursorStyle="|"
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+            //  onLoopDone={handleDone}
+            //  onType={handleType}
+          />
+        </h2>
 
         <Link to="/about">
           <button>Explore</button>
