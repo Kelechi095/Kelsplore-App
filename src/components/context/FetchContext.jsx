efb64d45fc700ad51ef89408ac360a7bcd680790
Awoms Kelechi <awomskelechi@gmail.com>
2023-05-21 20:04:47 +0100

    code refactoring

--- src/components/context/FetchContext.jsx
+++ src/components/context/FetchContext.jsx
@@ -4,7 +4,6 @@ import axios from "axios";
 const FetchContext = createContext();
 
 export const FetchProvider = ({ children }) => {
-
   const fetchMovies = async () => {
     const response = await axios.get(
       `https://api.themoviedb.org/3/trending/all/day?api_key=364d90c4eafb84407135bc900f697f3d&page=1`
@@ -14,12 +13,10 @@ export const FetchProvider = ({ children }) => {
 
   const fetchShows = async () => {
     const response = await axios.get(
-      `https://api.themoviedb.org/3/tv/popular?api_key=${APIKEY}&language=en-US&page=1`
+      `https://api.themoviedb.org/3/tv/popular?api_key=364d90c4eafb84407135bc900f697f3d&language=en-US&page=1`
     );
     return response.data;
   };
-
-
 
   return (
     <FetchContext.Provider
